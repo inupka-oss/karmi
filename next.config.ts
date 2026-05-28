c79b43f483fb77f6e809c6bf5ca92a5080df27cd
Karmi Admin <inupka@gmail.com>
2026-05-28 20:19:17 +0300

    Разрешена загрузка внешних изображений

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,14 @@
 import type { NextConfig } from "next";
-const nextConfig: NextConfig = {};
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',  // Разрешаем все домены (для простоты)
+      },
+    ],
+  },
+};
+
 export default nextConfig;
