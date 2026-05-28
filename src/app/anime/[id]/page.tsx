d0e47eb836976da8cee2cdfb27d5f03178c42de8
Karmi Admin <inupka@gmail.com>
2026-05-28 19:43:40 +0300

    проба 2

--- src/app/anime/[id]/page.tsx
+++ src/app/anime/[id]/page.tsx
@@ -1,8 +1,9 @@
-export default function AnimePage() {
+export default async function AnimePage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params   // просто чтобы показать, что мы обработали Promise
   return (
     <div className="max-w-5xl mx-auto px-4 py-8 text-white">
       <h1 className="text-3xl font-bold">Тест маршрута</h1>
-      <p>Если ты это видишь, маршрут /anime/[id] работает.</p>
+      <p>ID аниме: {id}</p>
     </div>
   )
 }
