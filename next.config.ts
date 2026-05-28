2d6b53b40a16134b7bf95d0357d0d099581780ef
Karmi Admin <inupka@gmail.com>
2026-05-28 17:31:17 +0300

    Добавлено игнорирование ошибок TypeScript

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
-    // Временно игнорируем ошибки TypeScript при сборке
     ignoreBuildErrors: true,
   },
 };
